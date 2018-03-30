8f536295beeb2c11678de9d2e48fb817364cadf1
Rodrigo Januario da Silva <rjdsilv@gmail.com>
2018-03-29 20:09:13 -0400

    Printing also ScriptableObjects

--- src/ca/rjdsilv/Main.java
+++ src/ca/rjdsilv/Main.java
@@ -1,26 +1,30 @@
 package ca.rjdsilv;
 
 import java.io.File;
+import java.util.Objects;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-	    File folder = new File("C:\\Users\\rjdsi\\GitHub\\COMP-305\\COMP305_FinalProject\\Assets\\Scripts");
-        listFilesForFolder(folder);
-    }
+	public static void main(String[] args) throws Exception {
+		System.out.println("######################################################## Scripts");
+		File folder = new File("C:\\Users\\rjdsi\\GitHub\\COMP-305\\COMP305_FinalProject\\Assets\\Scripts");
+		listFilesForFolder(folder, ".cs");
 
-    private static void listFilesForFolder(final File folder) throws Exception {
-        if (null != folder) {
-            for (final File fileEntry : folder.listFiles()) {
-                if (fileEntry.isDirectory()) {
-                    listFilesForFolder(fileEntry);
-                } else {
-                    String filePath = fileEntry.getCanonicalPath();
-                    if (filePath.endsWith(".cs")) {
-                        System.out.println(filePath.substring(filePath.indexOf("Scripts")));
-                    }
-                }
-            }
-        }
-    }
+		System.out.println("\n######################################################## Scriptable Objects");
+		folder = new File("C:\\Users\\rjdsi\\GitHub\\COMP-305\\COMP305_FinalProject\\Assets\\ScriptableObjects");
+		listFilesForFolder(folder, ".asset");
+	}
+
+	private static void listFilesForFolder(final File folder, String extension) throws Exception {
+		for (final File fileEntry : Objects.requireNonNull(folder.listFiles())) {
+			if (fileEntry.isDirectory()) {
+				listFilesForFolder(fileEntry, extension);
+			} else {
+				String filePath = fileEntry.getCanonicalPath();
+				if (filePath.endsWith(extension)) {
+					System.out.println(filePath.substring(filePath.indexOf("Assets")));
+				}
+			}
+		}
+	}
 }
